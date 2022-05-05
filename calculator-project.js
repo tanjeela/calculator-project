f20c6471a06c487ce3a0a41a0c6c87ae75a8ebf7
Anjum Tanjeela <tanjum@woolworths.com.au>
2022-05-05 19:13:16 +1000

    updated

--- calculator-project.js
+++ calculator-project.js
@@ -6,13 +6,12 @@
 // Created Array of Number Buttons
 
 let currentNumber = "";
-let previousNumber = ""
+let previousNumber = "";
 let operator = "";
-let displayValue ="0";
-let result = "";
+let displayValue ="";
 
-// Start on display
-const display = document.querySelector('#display-output')
+
+const display = document.querySelector('#display-output');
 
 const buttonsKeys = ["buttonNum0", "buttonNum1", "buttonNum2", "buttonNum3", "buttonNum4", "buttonNum5", "buttonNum6", "buttonNum7", "buttonNum8", "buttonNum9"]
 
@@ -22,36 +21,29 @@ for (let i = 0; i < buttonsKeys.length; i++) {
   const button = document.getElementById(buttonsKeys[i]);
   button.addEventListener('click', () => displayNumber(i))
   }
- 
 const displayNumber =(i) => {
     currentNumber += i;
     display.textContent = currentNumber;
 }
-// keeps track of operator
-// move current to previous number
-// equal current number nothing
 
 const inputOperator = (n) => {
   if (operator.length >= 1) 
   return; 
   operator = n; 
   display.textContent = operator;
-  currentNumber = previousNumber;
-  previousNumber = "";
+  previousNumber = currentNumber;
+  currentNumber = "";
 }
 
 // Operatorationzzzz//////
 
-
 //Division
 const division = document.getElementById("buttonDivision");
-division.addEventListener('click', () => inputOperator("÷"));
+division.addEventListener('click', () => inputOperator("/"));
 
 // Multiplication
-
 const multiplication = document.getElementById("buttonMultiplication");   
 multiplication.addEventListener('click', () => inputOperator("*"));
-
 
 //Subraction
 const subtraction = document.getElementById("buttonSubtract");
@@ -62,36 +54,42 @@ const addition = document.getElementById("buttonAddition");
 addition.addEventListener('click', () => inputOperator("+"));
 
 // AC Button
-const AutoclearButton = document.getElementById('buttonAutoClear');
+const AutoclearButton = document.getElementById("buttonAutoClear");
 AutoclearButton.addEventListener("click",() => clear() )
-
 const clear = () => {
  currentNumber = "";
  operator = "";
- display.textContent = displayValue;;
+ display.textContent = "";
 }
+// Equal Button
+const buttonEqual = document.getElementById("buttonEqual");   
+buttonEqual.addEventListener('click', () =>equals(previousNumber, operator, currentNumber));
+
+
+
 // THIS PART AIN'T WORKING FML 
   
-const buttonEqual = document.getElementById("buttonEqual");
+// const buttonEqual = document.getElementById("");
+// buttonEqual.addEventListener('click', event => 
 
-buttonEqual.addEventListener('click', event => {
-
+const equals = (previousNumber, operator, currentNumber) => {
+  currentNumber = parseFloat(currentNumber);
+  previousNumber = parseFloat(previousNumber);
+  let result = "";
   if (operator === "+") {
-    result = (parseInt(currentNumber)+(parseInt(previousNumber)));
-    display(result);
+    result = currentNumber + previousNumber;
+    display.textContent =(result);
   } else if (operator === "-") {
-    result = ((parseInt(currentNumber))-(parseInt(previousNumber)));
-    display(result);
-  } else if (operator === "÷") {
-    result = parseInt(currentNumber)/(parseInt(previousNumber));
-    display(result);
-  } else if (operator === "x") {
-    result = parseInt(currentNumber)*(parseInt(previousNumber))
-    display(result);
+    result = currentNumber - previousNumber;
+    display.textContent =(result);
+  } else if (operator === "/") {
+    result = currentNumber / previousNumber;
+    display.textContent = result;
+  } else if (operator === "*") {
+    result = currentNumber*previousNumber;
+    display.textContent =(result);
   } 
-})
+  return result;
 
-
-
-
+}
 
